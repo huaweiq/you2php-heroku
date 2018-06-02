716194901c2fb372972b2092e9b62994194c456c
huaweiq <39847597+huaweiq@users.noreply.github.com>
2018-06-02 17:51:16 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyD6r_o11nswFYWmzgt2dXLjze7AzUR0lH0p');//这里改成您的api key
 define('GJ_CODE', 'FR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', 'UTB');//网站名字
+define('TITLENAME', 'UTB');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF4352ffsfdqwgqwd02kfe67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'smallqiao4@gmail.com');//邮箱
 ?>
